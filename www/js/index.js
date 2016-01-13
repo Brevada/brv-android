1d8adeca3fd5330038423adc955623a3aa6f7d83
NoahNU <noahnu@gmail.com>
2016-01-12 23:36:38 -0500

    Fixed duplicate db insert.

--- www/js/index.js
+++ www/js/index.js
@@ -2,32 +2,34 @@
 	Brevada Tablet Framework
 */
 
-var globals = {
-	api : 'http://brevada.com/api/v1/',
-	key : 'inOkNRZSOayWOPy9vqYA',
-	store : '',
-	uuid : '',
-	device : null,
-	appDirURL : '',
-	battery : { level : 100, isPlugged : false },
-	adminPassword : 'Brevada!23',
-	db : null,
-	polling : false,
-	pollTmr : null,
-	pollInterval : 60000*5,
-	sessionToken : 'not-set',
-	
-	inactivityTmr : null,
-	inactiveDelayA : 45000,
-	inactiveDelayB : 15000,
-	inactiveA : null,
-	inactiveB : null
-};
-
-var backPushedTimes = 0;
-var backPushedTime = 0;
-
 var app = {
+	opts : {
+		connection : {
+			api : 'http://brevada.com/api/v1/',
+			key : 'inOkNRZSOayWOPy9vqYA',
+			payloadTimeout : 5000
+		},
+		admin : {
+			password : 'Brevada!23',
+			backPushedTimes : 0,
+			backPushedTime : 0
+		},
+		filesystem : {
+			store : '',
+			appDir : '',
+			db : null
+		},
+		system : {
+			uuid : '',
+			device : null,
+			battery : { level : 100, isPlugged : false }
+		},
+		polling : {
+			polling : false,
+			pollTmr : null,
+			pollInterval : 60000*5
+		}
+	},
     initialize: function() {
         $(document).on('deviceready', this.onDeviceReady);
     },
@@ -35,26 +37,25 @@ var app = {
 		$('#deviceready > p').html(msg);
 	},
     onDeviceReady: function() {
-		console.log("Device is ready.");
+		app.log("Device is ready.");
 		
 		app.preventExit();
 		
 		window.addEventListener("batterystatus", function(info){
-			globals.battery = info;
+			app.opts.system.battery = info;
 		}, false);
 		
-		console.log("Opening database...");
-		globals.db = window.sqlitePlugin.openDatabase({ name: "database.db", createFromLocation: 1 });
+		app.log("Opening database...");
+		app.opts.filesystem.db = window.sqlitePlugin.openDatabase({ name: "database.db", createFromLocation: 1 });
 		
 		app.configureEnv(function(){
 			app.update(function(){
 				app.start(function(){
 					// All good.
-					app.newSessionToken();
 					setTimeout(app.poll, 10000);
 				}, function(){
 					// Critical error.
-					app.status("Sorry, something went wrong.<br/>Please call 1-(844)-BREVADA.<br /><br />#"+globals.uuid);
+					app.status("Sorry, something went wrong.<br/>Please call 1-(844)-BREVADA.<br /><br />#"+app.opts.system.uuid);
 				});
 			});
 		}, function(){
@@ -63,27 +64,27 @@ var app = {
 		});
     },
 	preventExit : function(){
-		console.log("Preventing exit...");
+		app.log("Preventing exit...");
 		PreventExit.enable();
 		document.addEventListener('backbutton', function(e){
 			var now = (new Date()).getTime();
 			
-			if(now - backPushedTime > 3000){
-				backPushedTimes = 0;
+			if(now - app.opts.backPushedTime > 3000){
+				app.opts.backPushedTimes = 0;
 			}
 			
-			backPushedTimes++;
-			backPushedTime = now;
+			app.opts.backPushedTimes++;
+			app.opts.backPushedTime = now;
 			
-			if(backPushedTimes > 10){
-				console.log("Showing admin panel.");
+			if(app.opts.backPushedTimes > 10){
+				app.log("Showing admin panel.");
 				navigator.notification.prompt(
 					"Please enter the password, click 'OK', then the 'Home' button.",
 					function(results){
 						if(results.buttonIndex == 2){
-							backPushedTimes = 0;
+							app.opts.backPushedTimes = 0;
 						} else if(results.buttonIndex == 1){
-							if(results.input1 == globals.adminPassword){
+							if(results.input1 == app.opts.admin.password){
 								PreventExit.disable();
 								window.plugins.toast.showShortBottom("Access granted.");
 							} else {
@@ -103,21 +104,21 @@ var app = {
 	configureEnv : function(success, fail) {
 		app.status("Configuring environment.");
 		window.plugins.uniqueDeviceID.get(function(u){
-			globals.store = cordova.file.dataDirectory;
-			globals.device = document.getElementById('deviceProperties');
-			globals.uuid = u;
-			console.log("UUID = " + globals.uuid);
+			app.opts.filesystem.store = cordova.file.dataDirectory;
+			app.opts.system.device = document.getElementById('deviceProperties');
+			app.opts.system.uuid = u;
+			app.log("UUID = " + app.opts.system.uuid);
 			
 			window.resolveLocalFileSystemURL(cordova.file.applicationDirectory, function(entry){
-				globals.appDirURL = entry.toURL() + 'www/';
-				console.log('AppDirURL: ' + globals.appDirURL);
+				app.opts.filesystem.appDir = entry.toURL() + 'www/';
+				app.log('AppDirURL: ' + app.opts.filesystem.appDir);
 				success();
 			}, function(){
-				console.log('Failed resolving application directory.');
+				app.log('Failed resolving application directory.');
 				fail();
 			});
 		}, function(){
-			console.log('Failed.');
+			app.log('Failed.');
 			fail();
 		});
 	},
@@ -134,12 +135,12 @@ var app = {
 					css += app.resolvePaths(data);
 					app.loadStyles(styles, done, css);
 				}).fail(function(){
-					console.log("Failed to load: " + style);
-					app.status("Sorry, something went wrong.<br/>Please call 1-(844)-BREVADA.<br /><br />#"+globals.uuid);
+					app.log("Failed to load: " + style);
+					app.status("Sorry, something went wrong.<br/>Please call 1-(844)-BREVADA.<br /><br />#"+app.opts.system.uuid);
 				});
 			}, function(){
-				console.log("Failed to load: " + style);
-				app.status("Sorry, something went wrong.<br/>Please call 1-(844)-BREVADA.<br /><br />#"+globals.uuid);
+				app.log("Failed to load: " + style);
+				app.status("Sorry, something went wrong.<br/>Please call 1-(844)-BREVADA.<br /><br />#"+app.opts.system.uuid);
 			});
 		}
 	},
@@ -150,58 +151,58 @@ var app = {
 		if(stylesPath === false){
 			stylesPath = [];
 		}
-		console.log('Loading styles...');
+		app.log('Loading styles...');
 		app.loadStyles(stylesPath, function(){
 			var markupPath = app.getMarkupPath();
 			if(markupPath !== false){
-				console.log('Loading markup: ' + markupPath);
+				app.log('Loading markup: ' + markupPath);
 				window.resolveLocalFileSystemURL(markupPath, function(entry){
 					$.get(entry.toURL(), function(data){
-						console.log('Loaded markup.');
+						app.log('Loaded markup.');
 						data = app.resolvePaths(data);
 						$('#cordova-app').html(data);
 						
 						var scriptPath = app.getScriptPath();
 						if(scriptPath !== false){
-							console.log('Loading script...');
+							app.log('Loading script...');
 							window.resolveLocalFileSystemURL(scriptPath, function(en){
 								$.getScript(en.toURL(), function(){
-									console.log('Executing script...');
+									app.log('Executing script...');
 									success();
 								});
 							}, function(){
-								console.log('Script failed to load.');
+								app.log('Script failed to load.');
 								fail();
 							});
 						}
 					}, 'html').fail(function(){
-						app.status("Sorry, something went wrong.<br/>Please call 1-(844)-BREVADA.<br /><br />#"+globals.uuid);
+						app.status("Sorry, something went wrong.<br/>Please call 1-(844)-BREVADA.<br /><br />#"+app.opts.system.uuid);
 					});
 				}, function(){
-					console.log('Failed to load markup.');
+					app.log('Failed to load markup.');
 					fail();
 				});
 			}
 		});
 	},
 	update : function(done) {
-		console.log("Entering update process..");
+		app.log("Entering update process..");
 		
 		if(!app.online()){
-			console.log("Failed to check for updates: no internet connection.");
+			app.log("Failed to check for updates: no internet connection.");
 			done();
 			return;
 		}
 		
 		app.status("Checking for updates...");
 		$.ajax({
-			url : globals.api+'resources',
+			url : app.opts.connection.api+'resources',
 			cache : false,
 			dataType : 'json',
 			timeout : 7500,
-			data: {serial : globals.uuid},
+			data: {serial : app.opts.system.uuid},
 			success : function(data){
-				console.log(JSON.stringify(data));
+				app.log(JSON.stringify(data));
 				if(!data.hasOwnProperty('error') || data.error.length == 0){
 					if(data.hasOwnProperty('download') && data.download.length > 0){
 						app.downloadBatch(data.download, function(){
@@ -211,7 +212,7 @@ var app = {
 				} else { done(); }
 			}
 		}).fail(function(){
-			console.log("Failed to check for updates.");
+			app.log("Failed to check for updates.");
 			done();
 		});
 	},
@@ -221,25 +222,25 @@ var app = {
 		} else {
 			var dl = filesToDownload.shift();
 			
-			window.resolveLocalFileSystemURL(globals.store + dl.name, function(entry){
+			window.resolveLocalFileSystemURL(app.opts.filesystem.store + dl.name, function(entry){
 				Checksum.forFile(entry.toURL(), function(hex){
 					if(hex != dl.sha1){
-						console.log(dl.name + " is out of date.");
+						app.log(dl.name + " is out of date.");
 						app.downloadFile(dl, function(){
 							app.downloadBatch(filesToDownload, done);
 						});
 					} else {
-						console.log(dl.name + " is up-to-date.");
+						app.log(dl.name + " is up-to-date.");
 						app.downloadBatch(filesToDownload, done);
 					}
 				}, function(err){
-					console.log("Checksum failed for "+entry.toURL()+" : " + err);
+					app.log("Checksum failed for "+entry.toURL()+" : " + err);
 					app.downloadFile(dl, function(){
 						app.downloadBatch(filesToDownload, done);
 					});
 				});
 			}, function(){
-				console.log("New file available.");
+				app.log("New file available.");
 				app.status("Update found. Downloading...");
 				app.downloadFile(dl, function(){
 					app.downloadBatch(filesToDownload, done);
@@ -257,11 +258,11 @@ var app = {
 			app.status("Downloading new interface...");
 		}
 		
-		console.log("Entering download process.");
+		app.log("Entering download process.");
 		var fileTransfer = new FileTransfer();
-		console.log("Beginning file transfer.");
-		fileTransfer.download(dl.url, globals.store + dl.name, function(entry){
-			console.log('Downloaded to '+globals.store + dl.name+'.');
+		app.log("Beginning file transfer.");
+		fileTransfer.download(dl.url, app.opts.filesystem.store + dl.name, function(entry){
+			app.log('Downloaded to '+app.opts.filesystem.store + dl.name+'.');
 			if(dl.role == 'markup' || dl.role == 'script'){
 				localStorage.setItem(dl.role, entry.toURL());
 			} else if(dl.role == 'styles') {
@@ -276,7 +277,7 @@ var app = {
 			
 			done();
 		}, function(err){
-			console.log('Error downloading.');
+			app.log('Error downloading.');
 			done();
 		}, true);
 	},
@@ -300,68 +301,80 @@ var app = {
 	},
 	resolvePaths : function(content) {
 		content = content.replace(/(^[.])\/images/g, "/images");
-		content = content.replace("/images", globals.appDirURL+"images");
+		content = content.replace("/images", app.opts.filesystem.appDir+"images");
 		return content;
 	},
 	online : function(){
 		return navigator.connection.type != Connection.NONE && navigator.connection.type != Connection.UNKNOWN;
 	},
-	poll : function(){
-		clearTimeout(globals.pollTmr);
-		if(globals.polling){ return; }
-		globals.polling = true;
-		
-		if(app.online()){
-			globals.db.transaction(function(tx){
-				tx.executeSql("SELECT id, data FROM payloads LIMIT 50", [], function(tx,res){
-					for(var i = 0; i < res.rows.length; i++){
-						var row = res.rows.item(i);
-						var payload = JSON.parse(row.data);
-						app.sendPayload(payload, function(){
-							globals.db.executeSql("DELETE FROM payloads WHERE id = ?", [row.id], function(){
-								console.log("Row deleted.");
-							}, function(e){
-								console.log("DB Error: " + e.message);
-							});
-						});
-					}
-					globals.polling = false;
-					setTimeout(app.poll, globals.pollInterval);
-				}, function(e){
-					console.log("DB Error: " + e.message);
-					globals.polling = false;
-					setTimeout(app.poll, globals.pollInterval);
+	iteratePayloads : function(rows, index, done){
+		if(index < 0){
+			done();
+		} else {
+			var row = rows.item(index);
+			var payload = JSON.parse(row.data);
+			app.opts.filesystem.db.executeSql("DELETE FROM payloads WHERE id = ?", [row.id], function(){
+				app.log("Row deleted.");
+				app.sendPayload(payload, function(){
+					app.iteratePayloads(rows, index-1, done);
 				});
 			}, function(e){
-				console.log("DB Error: " + e.message);
-				globals.polling = false;
-				setTimeout(app.poll, globals.pollInterval);
+				app.log("DB Critical Error: " + e.message);
+				app.iteratePayloads(rows, index-1, done);
 			});
-			
+		}
+	},
+	poll : function(){
+		clearTimeout(app.opts.polling.pollTmr);
+		if(app.opts.polling.polling){ return; }
+		app.opts.polling.polling = true;
+		
+		if(app.online()){
+			app.opts.filesystem.db.transaction(function(tx){
+				tx.executeSql("SELECT id, data FROM payloads", [], function(tx,res){
+					if(res.rows.length > 0){
+						app.iteratePayloads(res.rows, res.rows.length-1, function(){
+							app.opts.polling.polling = false;
+							setTimeout(app.poll, app.opts.polling.pollInterval);
+						});
+					} else {
+						app.opts.polling.polling = false;
+						setTimeout(app.poll, app.opts.polling.pollInterval);
+					}
+				}, function(e){
+					app.log("DB Error A: " + e.message);
+					app.opts.polling.polling = false;
+					setTimeout(app.poll, app.opts.polling.pollInterval);
+				});
+			}, function(e){
+				app.log("DB Error B: " + e.message);
+				app.opts.polling.polling = false;
+				setTimeout(app.poll, app.opts.polling.pollInterval);
+			});
 		} else {
-			globals.polling = false;
-			setTimeout(app.poll, globals.pollInterval);
+			app.opts.polling.polling = false;
+			setTimeout(app.poll, app.opts.polling.pollInterval);
 		}
 	},
 	failedSubmission : function(payload) {
-		console.log("Failed to upload payload.");
+		app.log("Failed to upload payload.");
 		var payloadString = JSON.stringify(payload);
 		
-		globals.db.transaction(function(tx){
+		app.opts.filesystem.db.transaction(function(tx){
 			tx.executeSql("CREATE TABLE IF NOT EXISTS payloads (id integer primary key, data text)");
 			tx.executeSql("INSERT INTO payloads (data) VALUES (?)", [payloadString], function(tx, res){
 				if(res.rowsAffected == 1){
-					console.log("Stored payload.");
+					app.log("Stored payload.");
 				} else {
-					console.log("Error. Unable to store payload.");
+					app.log("Error. Unable to store payload.");
 				}
 			});
 		}, function(e){
-			console.log("DB Error: " + e.message);
+			app.log("DB Error C: " + e.message);
 		});
 	},
 	sendPayload : function(payload, sent){
-		if(!app.online()){
+		if(!app.online()){ app.log("Not online...");
 			app.failedSubmission(payload);
 			return;
 		}
@@ -371,29 +384,29 @@ var app = {
 		var keys = Object.keys(payload);
 		keys.sort();
 		
-		var dataString = globals.key;
+		var dataString = app.opts.connection.key;
 		for(var i = 0; i < keys.length; i++){
 			if(keys[i] == 'signature'){ continue; }
 			dataString += keys[i] + "=" + payload[keys[i]];
 		}
 		dataString = dataString.toLowerCase();
-		console.log("Data String: "+dataString);
+		app.log("Data String: "+dataString);
 		
 		Checksum.forString(dataString, function(hex){
 			payload.signature = hex;
-			console.log("Signature: "+hex);
+			app.log("Signature: "+hex);
 			
 			$.ajax({
-				url : globals.api+'feedback',
+				url : app.opts.connection.api+'feedback',
 				cache : false,
 				dataType : 'json',
-				timeout : 5000,
+				timeout : app.opts.connection.payloadTimeout,
 				method : 'POST',
 				data: payload,
 				success : function(data){
-					console.log(JSON.stringify(data));
+					app.log(JSON.stringify(data));
 					if(!data.hasOwnProperty('error') || data.error.length == 0){
-						console.log("Rating submitted.");
+						app.log("Rating submitted.");
 						if(typeof sent === 'function'){
 							sent();
 						}
@@ -402,35 +415,18 @@ var app = {
 					}
 				}
 			}).fail(function(j, textStatus){
-				console.log("Failed to post feedback: " + textStatus + " - " + j.responseText);
+				app.log("Failed to post feedback: " + textStatus + " - " + j.responseText);
 				app.failedSubmission(payload);
 			});
 		}, function(err){
-			console.log("Failed to generate hash for feedback.");
+			app.log("Failed to generate hash for feedback.");
 			app.failedSubmission(payload);
 		});
 	},
-	newSessionToken : function(){
-		var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		var result = '';
-		for (var i = 32; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-		console.log('Session Token: ' + result);
-		globals.sessionToken = result;
+	log : function(msg){
+		console.log(msg);
 	},
-	updateInteraction : function(){
-		if(globals.inactiveDelayA > 0){
-			clearTimeout(globals.inactivityTmr);
-			globals.inactivityTmr = setTimeout(app.inactive, globals.inactiveDelayA);
-		}
-	},
-	inactive : function(){
-		if(typeof globals.inactiveA === 'function' && typeof globals.inactiveB === 'function'){
-			globals.inactiveA();
-			
-			clearTimeout(globals.inactivityTmr);
-			globals.inactivityTmr = setTimeout(globals.inactiveB, globals.inactiveDelayB);
-		}
-	}
+	custom : { }
 };
 
 app.initialize();
